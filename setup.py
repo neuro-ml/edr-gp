293cf8f87d15575711832581782c89e4e2a28169
mibel <belyaevmichel@gmail.com>
2017-10-10 21:37:31 +0300

    version with fixed normilize=false

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ description = 'Effective Dimensionality Reduction based on Gaussian Processes'
 
 setup(
     name='edr-gp',
-    version='0.0.2',
+    version='0.0.3',
     description=description,
     long_description=long_description,
     url='https://github.com/neuro-ml/edr-gp',
