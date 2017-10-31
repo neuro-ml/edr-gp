9c59dfce6d78a3d36ebc8ae5a37de86989dd74c1
NikolayLutsyak <nikolay_96@bk.ru>
2017-10-31 11:29:27 +0300

    Update __init__.py
    Now we can do: from edrgp import EffectiveDimensionalityReduction
    instead of from edrgp.edr import  EffectiveDimensionalityReduction

---
+++ edrgp/__init__.py
@@ -0,0 +1,7 @@
+from .classification import GaussianProcessClassifier
+from .regression import GaussianProcessRegressor
+from .edr import EffectiveDimensionalityReduction
+
+__all__ = ['GaussianProcessClassifier',
+           'GaussianProcessRegressor',
+           'EffectiveDimensionalityReduction']
