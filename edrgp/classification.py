d51da4356753fb26944903616cdedfcb99e7981e
NikolayLutsyak <nikolay_96@bk.ru>
2017-11-01 12:53:56 +0300

    Add basic docstring to classification.py

--- edrgp/classification.py
+++ edrgp/classification.py
@@ -1,3 +1,5 @@
+"""Classes for classification based on GPy classification"""
+
 from sklearn.base import ClassifierMixin
 from GPy.models import GPClassification as _GPClassification
 from .base import _BaseGP
@@ -6,5 +8,6 @@ from .base import _BaseGP
 class GaussianProcessClassifier(_BaseGP, ClassifierMixin):
 
     def _get_model(self, X, y, kernel):
+        """Returns the GPy classification model with initialized params"""
         return _GPClassification(X, y, kernel, self.Y_metadata,
-                                self.mean_function)
+                                 self.mean_function)
