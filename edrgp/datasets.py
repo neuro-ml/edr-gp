659526f0e98616821f9b2868124a92872d3180db
mibel <belyaevmichel@gmail.com>
2017-11-13 10:36:14 +0300

    branin function is added

--- edrgp/datasets.py
+++ edrgp/datasets.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import special_ortho_group
 from sklearn.utils import check_array
+from math import pi
 
 
 def get_gaussian_inputs(sample_size, eig_values, eig_vectors=None, mean=None):
@@ -26,5 +27,39 @@ def get_tanh_targets(X, coefs, bias=0, noise_std=0.05):
         raise ValueError(
             'Dimensionality of input ({}) and coefs ({}) are mismatched')
     y = np.tanh(np.dot(X, coefs) + bias)
-    y += + noise_std * np.random.randn(X.shape[0])
+    y += noise_std * np.random.randn(X.shape[0])
+    return y
+
+
+def get_branin_targets(X, noise_std=None):
+    '''Branin function generator
+    It's a smooth 2D function which is frequently used as a target for
+    testing of optimization algorithms.
+    For details see https://www.sfu.ca/~ssurjano/branin.html
+
+    Parameters
+    ----------
+    X : np.array
+        The input data. There are two requirements:
+         - must be a 2D array with a shape [n_samples, 2]
+         - both columns should be in range [0, 1]
+    noise_std : float or None
+        Standard deviation of iid Gaussian noise which will be added to outputs
+
+    Returns
+    -------
+    y : np.array
+        Vector of target, [n_samples, ]
+    '''
+    # ToDo: check X
+
+    # params
+    a, b, c = 1, 5.1 / (4 * pi**2), 5 / pi
+    r, s, t = 6, 10, 1 / (8 * pi)
+    # x0 ∈ [-5, 10], x1 ∈ [0, 15].
+    x0 = 15 * X[:, 0] - 10
+    x1 = 15 * X[:, 1]
+    y = a * (x1 - b * x0**2 + c * x0 - r)**2 + s * (1 - t) * np.cos(x0) + s
+    if noise_std is not None:
+        y += noise_std * np.random.randn(X.shape[0])
     return y
