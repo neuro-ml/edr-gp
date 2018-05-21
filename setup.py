1f0ae86c6167d31427b447064d48b59394f0484c
NikolayLutsyak <nikolay_96@bk.ru>
2018-05-21 16:49:38 +0300

    new version of the lib:0.2.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ description = 'Effective Dimensionality Reduction based on Gaussian Processes'
 
 setup(
     name='edr-gp',
-    version='0.2.6',
+    version='0.2.7',
     description=description,
     long_description=long_description,
     url='https://github.com/neuro-ml/edr-gp',
