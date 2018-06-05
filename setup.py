6cc1019614c0e815ec7e2b6635a334b5c2419d14
NikolayLutsyak <nikolay_96@bk.ru>
2018-06-05 19:44:18 +0300

    Fix compatibility with all versions of pip

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # read dependencies from requirements.txt
 install_reqs = parse_requirements('requirements.txt', session=False)
